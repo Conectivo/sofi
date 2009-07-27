3e9b5a4a94e3acddc56a0dadf48e74bad0e1950d
aolivares <aolivares@localhost>
2009-07-27 10:17:28 +0000

    Agregada Paginaci?n de Eventos, por Joger Quintero <jogerq@gmail.com>.

--- sofi/apps/evento/urls.py
+++ sofi/apps/evento/urls.py
@@ -3,6 +3,6 @@ from django.views.generic.simple import direct_to_template
 
 
 urlpatterns = patterns('',
-    #url(r'^$', direct_to_template, {"template": "evento/evento.html"}, name="evento"),
     url(r'^$', 'evento.views.listEventos', name="evento"),
+    url(r'^(\d+)/$', 'evento.views.listEventos', name="evento"),
 )
