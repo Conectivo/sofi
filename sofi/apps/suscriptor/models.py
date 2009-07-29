29aebf442c6a347c0bb6566aa003916537fd8dbb
Alexander Olivares <olivaresa@gmail.com>
2009-07-29 16:53:34 +0000

    Agregada lista de pa?ses a formulario de eventos, modificaciones a reporte de suscriptores

--- sofi/apps/suscriptor/models.py
+++ sofi/apps/suscriptor/models.py
@@ -11,10 +11,12 @@ class Suscriptor(models.Model):
     profesion = models.CharField(max_length=21, blank=True, verbose_name='profesión')
     institucion = models.CharField(max_length=50, blank=True, verbose_name='institución')
     estado = models.CharField(max_length=15)
-    pais = models.CharField(max_length=10, verbose_name='país')
+    pais = models.CharField(max_length=58, verbose_name='país')
     evento = models.ForeignKey(Evento)
     
 
     def __unicode__(self):
         return "%s %s" % (self.nombres, self.apellidos)
     
+    class Meta:
+        ordering = ['-pais', '-estado', '-nombres', '-apellidos']
