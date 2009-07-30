202ca37f9e56decf025ab9a610b1a87f7efa22a2
Alexander Olivares <olivaresa@gmail.com>
2009-07-30 09:04:04 +0000

    Modificado modelo y formulario de suscriptores campo cedula cambiado a 12 d?gitos, cambio anual en la bd: ALTER TABLE suscriptor_suscriptor ALTER COLUMN cedula TYPE numeric(12, 0);

--- sofi/apps/suscriptor/models.py
+++ sofi/apps/suscriptor/models.py
@@ -6,7 +6,7 @@ from evento.models import Evento
 class Suscriptor(models.Model):
     nombres = models.CharField(max_length=21)
     apellidos = models.CharField(max_length=21)
-    cedula = models.IntegerField(max_length=8, verbose_name='cédula', unique=True)
+    cedula = models.DecimalField(verbose_name='cédula', unique=True, max_digits=12, decimal_places=0)
     email = models.EmailField()
     profesion = models.CharField(max_length=21, blank=True, verbose_name='profesión')
     institucion = models.CharField(max_length=50, blank=True, verbose_name='institución')
