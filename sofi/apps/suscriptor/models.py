176f3e51acb9580b58f38828d2ec14c134a83d75
aolivares <aolivares@localhost>
2009-08-11 17:18:32 +0000

    Modificado el modelo de suscriptor, eliminada la restricion de unique en el campo de cedula para permitir suscribir personas registradas en otros eventos.

--- sofi/apps/suscriptor/models.py
+++ sofi/apps/suscriptor/models.py
@@ -6,7 +6,7 @@ from evento.models import Evento
 class Suscriptor(models.Model):
     nombres = models.CharField(max_length=21)
     apellidos = models.CharField(max_length=21)
-    cedula = models.DecimalField(verbose_name='cédula', unique=True, max_digits=12, decimal_places=0)
+    cedula = models.DecimalField(verbose_name='cédula', max_digits=12, decimal_places=0)
     email = models.EmailField()
     profesion = models.CharField(max_length=21, blank=True, verbose_name='profesión')
     institucion = models.CharField(max_length=50, blank=True, verbose_name='institución')
@@ -16,7 +16,7 @@ class Suscriptor(models.Model):
     
 
     def __unicode__(self):
-        return "%s %s" % (self.nombres, self.apellidos)
+        return "%s %s - Evento: %s" % (self.nombres.capitalize(), self.apellidos.capitalize(), self.evento)
     
     class Meta:
         ordering = ['pais', 'nombres', 'apellidos']
