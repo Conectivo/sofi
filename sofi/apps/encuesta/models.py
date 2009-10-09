3b6685ae85e71ddbd2d3e0a9ee87a345131c86b6
Alexander Olivares <olivaresa@gmail.com>
2009-10-09 08:43:49 +0000

    Modificados datos de encuesta.

--- sofi/apps/encuesta/models.py
+++ sofi/apps/encuesta/models.py
@@ -14,10 +14,10 @@ MSPN = (
 class Encuesta(models.Model):
     item1 = models.IntegerField(verbose_name="1.1 Los contenidos desarrollados han sido pertinentes y coherentes a la temática planteada para el evento", choices=MSPN)
     item2 = models.IntegerField(verbose_name="1.2 Los contenidos manejados son aportes adecuados a la consolidación de la idea del Conocimiento Libre", choices=MSPN)
-    item3 = models.IntegerField(verbose_name="1.3 Los contenidos expuestos constituyen como herramienta de reforzamiento de la reflexión sobre la temática del Conocimiento Libre y Licenciamiento en América Latina", choices=MSPN)
+    item3 = models.IntegerField(verbose_name="1.3 Los contenidos expuestos constituyen herramienta de reforzamiento de la reflexión sobre la temática del Conocimiento Libre y Licenciamiento en América Latina", choices=MSPN)
 
     item4 = models.IntegerField(verbose_name="2.1 Los medios empleados han facilitado la interacción entre los participantes, conferencistas y ponentes", choices=MSPN)
-    item5 = models.IntegerField(verbose_name="2.2 Considera suficientes los medios empleados para promover la interacción entre los participantes", choices=MSPN)
+    item5 = models.IntegerField(verbose_name="2.2 Considera idoneos los medios empleados para promover la interacción entre los participantes", choices=MSPN)
     item6 = models.IntegerField(verbose_name="2.3 La moderación permitio la interacción de todos los participantes", choices=MSPN)
     item7 = models.IntegerField(verbose_name="2.4 La posibilidad de seguimiento del evento mediante actividades diferidas a través del canal video, archivos de audio facilito su participación en el evento", choices=MSPN)
     item8 = models.IntegerField(verbose_name="2.5 Emplear herramientas web, como la utilizadas en el evento democratiza el acceso a la información", choices=MSPN)
@@ -29,7 +29,7 @@ class Encuesta(models.Model):
     item12 = models.IntegerField(verbose_name="4.4 La plataforma del evento estuvo disponible durante todo el tiempo en que se desarrollo el evento", choices=MSPN)
     item13 = models.IntegerField(verbose_name="4.5 Fue sencillo el acceso a las herramientas empleadas para el desarrollo del evento (blog, inscripciones, aulavirtual, foros, canales de tv, repositorios de audio)", choices=MSPN)
     
-    item14 = models.IntegerField(verbose_name="5 Calificaría usted el desarrollo general del evento como un accionar colaborativo y cooperativo que ha generado insumos a la construcción de la idea del Conocimiento como Bien Público", choices=MSPN)
+    item14 = models.IntegerField(verbose_name="5 Calificaría usted el desarrollo general del evento como un accionar colaborativo y cooperativo que ha generado insumos a la construcción de la idea del Conocimiento como Bien Público en América Latina", choices=MSPN)
     
     fecha = models.DateField()
     suscriptor = models.ForeignKey(Suscriptor)
