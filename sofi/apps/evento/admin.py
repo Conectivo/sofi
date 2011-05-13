cada758da362039a61c7d70dbb8962749d04f43b
aolivares <aolivares@localhost>
2011-05-13 20:27:12 +0000

    Agregando usuario y fecha a eventos

--- sofi/apps/evento/admin.py
+++ sofi/apps/evento/admin.py
@@ -2,7 +2,8 @@ from evento.models import Evento
 from django.contrib import admin
 
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'presentaciones', 'suscripciones','publicar', 'fecha_ini']
+    list_display = ['nombre', 'fecha_ini', 'fecha_fin', 'presentaciones', 'suscripciones','publicar']
     search_fields = ['nombre', 'fecha_ini']
-
+    exclude = ('fecha', 'admin')
+    
 admin.site.register(Evento, EventoAdmin)
