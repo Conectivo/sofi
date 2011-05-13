e11f1b2852d49bc79e7e4b3a0a4812a13a6e7765
Alexander Olivares <olivaresa@gmail.com>
2011-05-13 20:27:12 +0000

    Agregando usuario y fecha a eventos

--- sofi/apps/evento/models.py
+++ sofi/apps/evento/models.py
@@ -1,10 +1,18 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from tools.thumbs import ImageWithThumbsField
+from django.utils.translation import ugettext as _
+from django.contrib.auth.models import User
+from django.db.models import signals
+from django.dispatch import Signal
+from datetime import datetime
 
 SINO = (
     (True, 'Si'),
     (False, 'No')
 )
+
 
 class Evento(models.Model):
     nombre = models.CharField(max_length=120)
@@ -15,15 +23,34 @@ class Evento(models.Model):
     suscripciones = models.BooleanField(choices=SINO)
     publicar = models.BooleanField(choices=SINO)
     comentario = models.BooleanField(default=True)
-    fecha_ini = models.DateField(verbose_name='fecha inicial')
-    fecha_fin = models.DateField(verbose_name='fecha final')
-    #logo = models.ImageField(upload_to='evento/files')
+    fecha = models.DateField(verbose_name=_(u'fecha publicación'))
+    fecha_ini = models.DateField(verbose_name=_('fecha inicial'))
+    fecha_fin = models.DateField(verbose_name=_('fecha final'))
     logo = ImageWithThumbsField(upload_to='evento/files', sizes=((180,150),))
-    patrocinio = models.BooleanField(verbose_name='Patrocinio a Suscriptores')
-
+    admin = models.ForeignKey(User)
+    
     def __unicode__(self):
         return self.nombre
     
     class Meta:
         ordering = ['-fecha_ini', '-fecha_fin']
+
+
+
+    
+     
+
+usuario = None
+
+def get_user(sender, instance, signal, * args, **kwargs):
+    global usuario
+    usuario = instance
+
+def guardar(sender, instance, signal, * args, **kwargs):
+    instance.fecha = datetime.now()
+    instance.admin = usuario
+    
+signals.post_init.connect(get_user, User)    
+signals.pre_save.connect(guardar, Evento)
+
 
