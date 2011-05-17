444db8471a01d22a483c388371f8c5f64de28396
aolivares <aolivares@localhost>
2011-05-17 15:03:09 +0000

    Varias modificaiones para alcanzar la ver 2.0

--- sofi/apps/evento/models.py
+++ sofi/apps/evento/models.py
@@ -7,18 +7,18 @@ from django.contrib.auth.models import User
 from django.db.models import signals
 from django.dispatch import Signal
 from datetime import datetime
-
-SINO = (
-    (True, 'Si'),
-    (False, 'No')
-)
-
+from tools.constantes import SINO
 
 class Evento(models.Model):
     nombre = models.CharField(max_length=120)
     resumen = models.TextField()
     lugar = models.TextField(blank=True)
     email = models.EmailField()
+    cta_twitter = models.CharField(max_length=50, blank=True, verbose_name=_(u'twitter'))
+    pass_twitter = models.CharField(max_length=12, blank=True, verbose_name=_(u'contraseña twitter'))
+    cta_facebook = models.CharField(max_length=50, blank=True, verbose_name=_(u'facebook'))
+    pass_facebook = models.CharField(max_length=12, blank=True, verbose_name=_(u'contraseña facebook'))
+    publicar_tf = models.BooleanField(choices=SINO, verbose_name=_(u'publicar en redes sociales'))
     presentaciones = models.BooleanField(choices=SINO)
     suscripciones = models.BooleanField(choices=SINO)
     publicar = models.BooleanField(choices=SINO)
@@ -28,6 +28,7 @@ class Evento(models.Model):
     fecha_fin = models.DateField(verbose_name=_('fecha final'))
     logo = ImageWithThumbsField(upload_to='evento/files', sizes=((180,150),))
     admin = models.ForeignKey(User)
+
     
     def __unicode__(self):
         return self.nombre
