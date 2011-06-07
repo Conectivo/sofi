cc1fb8d98def52538d34f0ef66c612f0a32146fb
aolivares <aolivares@localhost>
2011-06-07 15:19:06 +0000

    Add jquery.qtip to tooltips messges

--- sofi/apps/configuracion/models.py
+++ sofi/apps/configuracion/models.py
@@ -23,8 +23,8 @@ class Enlaces(models.Model):
         verbose_name_plural = _(u'Enlaces')
 
     
-Site.add_to_class('titulo', models.CharField(max_length=100, verbose_name=_(u'título')))
-Site.add_to_class('subtitulo', models.CharField(max_length=100, verbose_name=_(u'subtítulo')))
+Site.add_to_class('titulo', models.CharField(max_length=20, verbose_name=_(u'título')))
+Site.add_to_class('subtitulo', models.CharField(max_length=20, verbose_name=_(u'subtítulo')))
 Site.add_to_class('informacion', models.TextField(verbose_name=_(u'información')))
 Site.add_to_class('logo', ImageWithThumbsField(upload_to='configuracion/files', sizes=((120,100),), verbose_name=_(u'logo')))
 Site.add_to_class('banner', ImageWithThumbsField(upload_to='configuracion/files', sizes=((160,600),), verbose_name=_(u'banner 160x600')))
