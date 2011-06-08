2dc5e04fa6865920e65a05d1ae4a2653895d82f5
aolivares <aolivares@localhost>
2011-06-08 16:26:35 +0000

    pre encuesta

--- sofi/apps/encuesta/admin.py
+++ sofi/apps/encuesta/admin.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-from encuesta.models import Encuesta
+from encuesta.models import Encuesta, Items
 from django.contrib import admin
 
+class ItemsInline(admin.TabularInline):
+    model = Items
+
+
 class EncuestaAdmin(admin.ModelAdmin):
-    list_display = ('suscriptor', 'evento', 'fecha')
+    list_display = ('nombre', 'evento')
     list_filter = ('evento',)
-    search_fields = ('^suscriptor__nombres', '^suscriptor__apellidos')
+    #search_fields = ('^suscriptor__nombres', '^suscriptor__apellidos')
+
+    inlines = [
+            ItemsInline,
+        ]
     
 admin.site.register(Encuesta, EncuestaAdmin)
 
