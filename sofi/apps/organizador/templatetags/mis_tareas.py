ebd77b453f33357bab50af8f5d593134e147052e
Alexander Olivares <olivaresa@gmail.com>
2011-07-06 15:45:50 -0530

    modified report task execution rate added.

--- sofi/apps/organizador/templatetags/mis_tareas.py
+++ sofi/apps/organizador/templatetags/mis_tareas.py
@@ -10,6 +10,13 @@ register = template.Library()
 
 @register.inclusion_tag("organizador/mis_tareas.html")
 def show_tareas(user):
+    
     suscriptor_profile = UserProfile.objects.get(user=user)
     tareas = suscriptor_profile.organizador_set.all()
+        
     return {'mis_tareas': tareas,}
+
+@register.simple_tag
+def show_avg(evento):
+    from django.db.models import Avg
+    return evento.tarea_set.all().aggregate(Avg('porcentaje'))['porcentaje__avg']
