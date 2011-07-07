6979238492d1c1eaf25a822a460902cf596b8191
Alexander Olivares <olivaresa@gmail.com>
2011-07-07 08:52:59 -0530

    add tasks update form

--- sofi/apps/organizador/urls.py
+++ sofi/apps/organizador/urls.py
@@ -3,5 +3,5 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     url(r'^mis_tareas/$', direct_to_template, {'template': 'organizador/tareas.html'}),
-    #url(r'^mis_tareas/$', 'organizador.views.tareas'),
+    url(r'^actualizar_tarea/(\d+)/$', 'organizador.views.actualizar_tarea'),
 )
