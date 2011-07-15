8c20a8c008bd1adc7a088ed86c1b43037855fc25
Alexander Olivares <olivaresa@gmail.com>
2011-07-15 11:01:06 -0530

    Nested feedback functionality added up to three levels

--- sofi/apps/comentario/models.py
+++ sofi/apps/comentario/models.py
@@ -7,35 +7,38 @@ from django.contrib.comments.models import Comment
 from evento.models import Evento
 from django.contrib.sites.models import Site
 from tools import email
+from django.utils.translation import ugettext as _
 
 # Los comentarios no se publican automaticamente
 def pre_save_comment(sender, instance, **kwargs):
     if not instance.id:
         instance.is_public = False
 
-#Despues de cambiar el estado del comentario envia emails
+##Despues de cambiar el estado del comentario envia emails
 def post_save_comment(sender, instance, **kwargs):
-    evento = Evento.objects.get(id=instance.object_pk)
+
+    
     site = Site.objects.get(id=1)
 
-    if instance.is_public:
-        try:
-            if instance.content_type.name == "evento":
-                asunto = u'Comentario aprobado'
-                mensaje = u'Su comentario sobre el evento "%s" ha sido aprobado y publicado en:\nhttp://%s/comentario/%s/\n\nGracias...' % (evento.nombre, site.domain, evento.id)
-                email.enviar_mail(asunto, mensaje, evento.email, [instance.user_email])
-                
-        except Exception, error:
-            pass
-    else:
-        try:
-            if instance.content_type.name == "evento":
-                asunto = u'Nuevo comentario en lista de moderación'
-                mensaje = u'%s ha comentado el evento "%s":\n------\n%s\n------\nPara aprobar por favor haga click en la siguiente dirección:\nhttp://%s/admin/comments/comment/%s/' % (instance.user_name, evento.nombre, instance.comment, site.domain, instance.id)
-                email.enviar_mail(asunto, mensaje, evento.email, [evento.email])
-                
-        except Exception, error:
-            pass
+    #if instance.is_public:
+    #    asunto = _('Comentario aprobado')
+    #    para = [instance.user_email]
+    #else:
+    #    asunto = _(u'Nuevo comentario en lista de moderación')
+    #    para =  [evento.email]
+    #    
+    #if instance.content_type.name == 'comment':
+    #    comentario = Comment.objects.get(id=instance.object_pk)
+    #    mensaje = ''
+    #if instance.content_type.name == 'evento':
+    #    evento = Evento.objects.get(id=instance.object_pk)
+    #    mensaje = _('Su comentario sobre el evento') + '" ' + evento.nombre + '" ' + _('ha sido aprobado y publicado en:') + '\n' + site.domain + '/comentario/' +  evento.id + '/\n\n' + _('Gracias...')
+
+    #try:
+    #    email.enviar_mail(asunto, mensaje, evento.email, para)
+    #except Exception, error:
+    #    pass
+
 
 pre_save.connect(pre_save_comment, sender=Comment)
 post_save.connect(post_save_comment, sender=Comment)
