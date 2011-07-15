138a76dbe070f29e62c32960fd7e88abff0c7c54
Alexander Olivares <olivaresa@cantv.net>
2011-07-15 11:01:06 -0530

    Nested feedback functionality added up to three levels

--- sofi/apps/comentario/views.py
+++ sofi/apps/comentario/views.py
@@ -7,7 +7,7 @@ from django.template import RequestContext
 def comentario(request, evento):
     if Evento.objects.filter(id=evento):
         evento = Evento.objects.get(id=evento)
-        return render_to_response('comentario/comentario.html', {'evento': evento}, context_instance=RequestContext(request))
+        return render_to_response('comentario/comentario.html', {'objeto': evento, 'evento': evento}, context_instance=RequestContext(request))
     else:
         raise Http404
     
