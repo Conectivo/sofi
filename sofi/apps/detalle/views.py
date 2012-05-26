014f63fc997437f246f81950da61a59169625975
satanas <wil.alejandro@gmail.com>
2012-05-25 21:17:41 -0300

    Se cambia el bot?n de suscribir cuando el usuario ya esta suscrito al evento

--- sofi/apps/detalle/views.py
+++ sofi/apps/detalle/views.py
@@ -4,8 +4,12 @@ from django.contrib.sites.models import Site
 from django.template import RequestContext
 
 
+from suscriptor.models import UserProfile
+
+
 def show(request, id, template_name='evento/evento.html'):
     evento = Evento.objects.get(id=id)
-
-    template = render_to_response(template_name, {'evento': evento, 'site_name': Site.objects.get(id=1).name}, context_instance=RequestContext(request))
+    suscriptor_profile = UserProfile.objects.get(id=request.user.id)
+    is_suscribed = suscriptor_profile.is_suscribed(evento)
+    template = render_to_response(template_name, {'evento': evento, 'is_suscribed': is_suscribed, 'site_name': Site.objects.get(id=1).name}, context_instance=RequestContext(request))
     return template
